641f9ba598c051a51bff4c708895609e7007c1ef
RaghavaDhanya <raghava.dhanya@gmail.com>
2017-04-12 02:25:04 +0530

    pgm7color

--- pgm7color.c
+++ pgm7color.c
@@ -1,23 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-void printchar(char*);
+void printchar(char*,int);
 int main(void)
 {
 	pid_t pid;
-	if((pid==fork())<0)
+	if((pid=fork())<0)
 	printf("fork error\n");
 	else if(pid==0)
-	printchar("output from child\n");
+	printchar("output from child\n",222);
 	else
-	printchar("OUTPUT FROM PARENT\n");
+	printchar("OUTPUT FROM PARENT\n",32);
 	exit(0);
 }
-void printchar(char *str)
+void printchar(char *str,int color)
 {
 	char *ptr;
 	int c;
 	setbuf(stdout,NULL);
 	for(ptr=str;(c=*ptr)!=0;++ptr)
-	putc(c,stdout);
+	printf("\033[38;5;%dm%c\033[0;0;m",color,c);
 }
